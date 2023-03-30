62d1840e18ac655cd36a33d4cd89b23ecfef11e2
ajmdotdev <contact.ajmendoza@gmail.com>
2023-03-30 14:25:00 +0800

    add feature: create new todo item

--- my-app-fe/src/components/TodoCreate/TodoCreate.js
+++ my-app-fe/src/components/TodoCreate/TodoCreate.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTodos } from '../../redux/actions/todoAction';
 
 function TodoCreate() {
+    const [todoTitle, setTodoTitle] = useState('');
+    const [todoDescription, setTodoDescription] = useState('');
+    const allTodos = useSelector((state) => state.allTodos.todos)
+    const dispatch = useDispatch();
+
+    const addTodo = () => {
+        const newTodo = [...allTodos];
+
+        newTodo.push({
+            id:Math.floor(Math.random() * 20000),
+            title: todoTitle,
+            description: todoDescription
+        });
+
+        // Pag tapos na yung todo, kailangan nang ibalik/ipasa sa kabila
+        dispatch(setTodos(newTodo));
+    }
+
     return (
         <>
          <div className="container-fluid">
@@ -14,6 +34,7 @@ function TodoCreate() {
                     type="text"
                     name="title"
                     id="title"
+                    onChange={(e) => setTodoTitle(e.target.value)}
                     className="form-control"
                     placeholder="Enter title"
                 />
@@ -25,13 +46,14 @@ function TodoCreate() {
                     type="text"
                     name="description"
                     id="description"
+                    onChange={(e) => setTodoDescription(e.target.value)}
                     className="form-control"
                     placeholder="Enter description"
                 />
                 </div>
             </div>
             <div className="col-12 mt-2 d-flex justify-content-end">
-                <button className="btn btn-primary">ADD</button>
+                <button onClick={() => addTodo()} className="btn btn-primary">ADD</button>
             </div>
             </div>
         </div>   
